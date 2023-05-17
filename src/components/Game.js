bf79ac4c47f466e27a9ed4d9ee98c5f179a4407c
Benfi4rthur <arthur_benfica@hotmail.com>
2023-05-17 00:30:57 -0300

    css atualizado

--- src/components/Game.js
+++ src/components/Game.js
@@ -24,7 +24,7 @@ const Game = ({
             <h1>Adivinhe a palavra:</h1>
             <h3 className='tipo'>
                 Dica sobre a palavra: <span>{pegaCategoria}</span>
-                <p>Você ainda tem <span className='chances'>{chances}</span> tentativa(s).</p>
+                <p>Você ainda tem <span className={chances < 5 && chances >= 3 ? 'shake' : 'chances' && chances < 3 ? 'danger' : 'chances'}>{chances}</span> tentativa(s).</p>
             </h3>
 
             <div className='ContainerPalavras'>
